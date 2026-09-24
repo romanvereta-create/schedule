c01a07f9505a8b3059af4a2852a797f809c18754
romanvereta-create <romanvereta@gmail.com>
2026-09-24 17:08:38 +0300

    Pin bootstrap to immediate payment release

--- candidate_bootstrap.py
+++ candidate_bootstrap.py
@@ -15,7 +15,7 @@ import urllib.request
 from pathlib import Path, PurePosixPath
 
 
-PINNED_COMMIT = "aca24ef107a7b842c2b8af62ef4e4f9cfa95f846"
+PINNED_COMMIT = "9fc3f0c59edc9ac5ca691881cc4d8a4c2e2e6d32"
 RAW_BASE = (
     "https://raw.githubusercontent.com/romanvereta-create/schedule/"
     + PINNED_COMMIT
