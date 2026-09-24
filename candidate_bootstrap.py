d655ca274434258521f3ca6fb2e1e7d6a17bbd41
romanvereta-create <romanvereta@gmail.com>
2026-09-24 22:30:07 +0300

    Pin candidate diagnostics release

--- candidate_bootstrap.py
+++ candidate_bootstrap.py
@@ -15,7 +15,7 @@ import urllib.request
 from pathlib import Path, PurePosixPath
 
 
-PINNED_COMMIT = "6da4a2ca8a3dcee2eff9f3fb0786a9e574bb8017"
+PINNED_COMMIT = "18e1c2f9996f2d356540067288c5ee340db83901"
 RAW_BASE = (
     "https://raw.githubusercontent.com/romanvereta-create/schedule/"
     + PINNED_COMMIT
